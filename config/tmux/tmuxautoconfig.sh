0f1b2e71d59eb490ae6c390f1e46b13384ecb93c
Archlinux xfce4-lili <noemail@example.com>
2018-05-01 18:19:08 +0000

    modified tmuxautoconfig.sh

--- config/tmux/tmuxautoconfig.sh
+++ config/tmux/tmuxautoconfig.sh
@@ -2,9 +2,12 @@
 
 echo "=====This is a auto-script about tmux====== "
 echo "\n\ngit clone resurrect====>"
-git clone https://github.com/tmux-plugins/tmux-resurrect.git
+git clone https://github.com/tmux-plugins/tmux-resurrect.git ~/.tmux/tmux-resurrect
 echo -e "git clone continuum====>"
-git clone https://github.com/tmux-plugins/tmux-continuum.git
+git clone https://github.com/tmux-plugins/tmux-continuum.git ~/.tmux/tmux-continuum
 echo -e "\n\ncp .tmux.conf ~/====>"
 cp .tmux.conf ~/
 echo "done"
+echo "source file====>"
+tmux source-file ~/.tmux.conf
+echo "Enjoy"
