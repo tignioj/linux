3ebbeb1b8b903415204ff6aec9b7c57447d414ec
Arch-xfce-Virtual <noemail@example.com>
2018-05-09 16:40:06 +0800

    change config from vundle to spacevim

--- config/vim/vimautoconfig.sh
+++ config/vim/vimautoconfig.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+#
+#echo "========This is vim auto config script=========="
+#	echo "git clone vundle====>"
+#	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#	echo "done"
+#	echo -e "\n\ncp .vimrc ~/====>"
+#	cp .vimrc ~/
+#	echo "done"
+#	echo -e "\n\nInstalling plugins====>"
+#	vim +PluginInstall +qall
+#	echo "done"
+#	echo "Enjoy"
 
-echo "========This is vim auto config script=========="
-	echo "git clone vundle====>"
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	echo "done"
-	echo -e "\n\ncp .vimrc ~/====>"
-	cp .vimrc ~/
-	echo "done"
-	echo -e "\n\nInstalling plugins====>"
-	vim +PluginInstall +qall
-	echo "done"
-	echo "Enjoy"
+echo -e "\n\nspacevim=================>>\n\n"
+curl -sLf https://spacevim.org/install.sh | bash
